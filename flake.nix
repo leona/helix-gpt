1555192a74213b182a02205e548caa5f62921bd5
Wolfgang Senfter <w.senfter@gmail.com>
2024-03-11 20:25:13 +0100

    Added default package

--- flake.nix
+++ flake.nix
@@ -14,38 +14,59 @@
             config.allowUnfree = true;
           };
 
+          node_modules = with pkgs; stdenv.mkDerivation {
+            name = "helix-gpt_node_modules";
+            src = ./.;
 
-          node_modules =
-            let
-              inherit (pkgs) stdenv lib;
-            in
-            stdenv.mkDerivation {
-              name = "helix-gpt-node_modules";
-              src = ./.;
-              dontUnpack = true;
-              sourceRoot = ./.;
+            dontConfigure = true;
+            dontFixup = true;
+            nativeBuildInputs = [ bun ];
+            buildInputs = [ nodejs_20 ];
+            impureEnvVars = lib.fetchers.proxyImpureEnvVars ++ [ "GIT_PROXY_COMMAND" "SOCKS_SERVER" ];
 
-              dontConfigure = true;
-              nativeBuildInputs = [ pkgs.bun ];
-              impureEnvVars = lib.fetchers.proxyImpureEnvVars ++ [ "GIT_PROXY_COMMAND" "SOCKS_SERVER" ];
+            buildPhase = ''
+              bun install --no-progress --frozen-lockfile
+            '';
 
-              buildPhase = ''
-                echo "HELLO";
-                bun install --no-progress --frozen-lockfile
-              '';
+            installPhase = ''
+              mkdir -p $out/node_modules
 
-              installPhase = ''
-                mkdir -p $out/node_modules
+              cp -R ./node_modules $out
+            '';
 
-                cp -R ./node_modules $out
-              '';
-            };
+            outputHash = "sha256-y6K5Xq1Q5iK9xiXr4+kQSyDa9kxeYpdhmoBooeXm6hg=";
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+          };
+          helix-gpt = with pkgs; stdenv.mkDerivation {
+
+            name = "helix-gpt";
+            src = ./.;
+
+            dontConfigure = true;
+            dontBuild = true;
+            nativeBuildInputs = [ makeBinaryWrapper ];
+            buildInputs = [ bun ];
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/bin
+
+              ln -s ${node_modules}/node_modules $out
+              cp -R ./* $out
+
+              makeBinaryWrapper ${bun}/bin/bun $out/bin/helix-gpt \
+                --prefix PATH : ${lib.makeBinPath [ bun ]} \
+                --add-flags "run --prefer-offline --no-install --cwd $out ./src/app.ts"
+
+              runHook postInstall
+            '';
+          };
         in
         {
-          packages = rec {
-            inherit node_modules;
-
-            default = node_modules;
+          packages = {
+            default = helix-gpt;
           };
 
           devShells.default = with pkgs; mkShell {
